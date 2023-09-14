05cb35ab36141a6f7c673775a9a462c08dc797ce
SnakeyEye497 <105121030+SnakeyEye497@users.noreply.github.com>
2023-09-14 19:07:49 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+num=0
+while num != 1:
+            star = input()
+            if star=="/start":
+
+                 print("ENTER YOUR CHOICE")
+                 print("1.Admission enq  2.pay fees")
+                 choic1=int(input())
+                 num=1
+                 if choic1==1:
+                     print("*******")
+                 elif choic1==2:
+                     print("---------")
+                 else:
+                     print("Invalid choice")
+            else:
+                 print("\n"*100)
+
+                 num=0
+num=0
+while num != 1:
+            star = input()
+            if star=="/start":
+
+                 print("ENTER YOUR CHOICE")
+                 print("1.Admission enq  2.pay fees")
+                 choic1=int(input())
+                 num=1
+                 if choic1==1:
+                     print("*******")
+                 elif choic1==2:
+                     print("---------")
+                 else:
+                     print("Invalid choice")
+            else:
+                 print("\n"*100)
+
+                 num=0
